71044e5d3c549be67e73c3f68909d7e9b1a79e89
Arvind <arvindri2005@gmail.com>
2022-10-24 12:31:05 +0530

    Bottom Navigation Bar Added

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 Android app for anwesha2k23
 !!Must follow these instructions.
 
-Fork the repo. Clone it using gitbash. Work on a branch and push your branch to remote repo.(It would be good if you have github desktop for laptop)
+Fork the repo. Clone it using git bash. Work on a branch and push your branch to remote repo.(It would be good if you have github desktop for laptop)
 
-Search on Internet about how to fork a repo and how to pull from specific branches, how to puch a branch.
+Search on Internet about how to fork a repo and how to pull from specific branches, how to push a branch.
 Don't mashup the project.
 
 Just checking
--- app/src/main/java/com/college/anwesha2k23/MainActivity.kt
+++ app/src/main/java/com/college/anwesha2k23/MainActivity.kt
@@ -2,10 +2,48 @@ package com.college.anwesha2k23
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.college.anwesha2k23.Gallery
+import com.college.anwesha2k23.Home
+import com.college.anwesha2k23.Profile
+import com.college.anwesha2k23.R.*
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private  lateinit var bottomNav: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(layout.activity_main)
+        supportActionBar?.hide() //Hide App title
+        loadFragment(Home())
+        bottomNav = findViewById(id.bottomNavigation)
+        bottomNav.setOnItemSelectedListener {
+            when(it.itemId){
+                id.home -> {
+                    loadFragment(Home())
+                    true
+                }
+                id.gallery -> {
+                    loadFragment(Gallery())
+                    true
+                }
+                id.profile -> {
+                    loadFragment(Profile())
+                    true
+                }
+                else->{
+                    loadFragment(Home())
+                    true
+                }
+            }
+        }
+    }
+    private fun loadFragment(fragment: Fragment){
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(id.fragmentContainer, fragment)
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit()
     }
 }
