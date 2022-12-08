e36781592f79e71d0bf32edf9a5a5ce1737bf029
Divit <divitajmera15@gmail.com>
2022-12-08 11:20:55 +0530

    added api for campus ambassador

--- app/src/main/java/com/college/anwesha2k23/campusAmbassador/CaRegisterFragment.kt
+++ app/src/main/java/com/college/anwesha2k23/campusAmbassador/CaRegisterFragment.kt
@@ -1,12 +1,25 @@
 package com.college.anwesha2k23.campusAmbassador
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.StrictMode
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.college.anwesha2k23.R
 import com.college.anwesha2k23.databinding.FragmentCaRegisterBinding
+import com.google.android.material.snackbar.BaseTransientBottomBar.ANIMATION_MODE_SLIDE
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputLayout
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import okhttp3.MediaType
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
+
 
 class CaRegisterFragment : Fragment() {
     private lateinit var binding: FragmentCaRegisterBinding
@@ -18,11 +31,67 @@ class CaRegisterFragment : Fragment() {
         val view = binding.root
 
         binding.registerCA.setOnClickListener {
-            showFragment()
+            val name = checkValue(binding.name) ?: return@setOnClickListener
+            val email = checkValue(binding.emailId) ?: return@setOnClickListener
+            val contact = checkValue(binding.number) ?: return@setOnClickListener
+            val college = checkValue(binding.college) ?: return@setOnClickListener
+            val years = checkValue(binding.year) ?: return@setOnClickListener
+            val referral = checkValue(binding.referral) ?: return@setOnClickListener
+            val pass = checkValue(binding.password) ?: return@setOnClickListener
+            val confirmPass = checkValue(binding.cPassword) ?: return@setOnClickListener
+            if(pass != confirmPass) {
+                binding.cPassword.error = "Password does not match!"
+                return@setOnClickListener
+            }
+            binding.cPassword.error = null
+
+
+            Thread {
+                val client = OkHttpClient()
+                val mediaType = "application/json".toMediaTypeOrNull()
+                val data =
+                    "{\n  \"password\" : \"${pass}\",\n  \"phone_number\":  \"$contact\" ,\n  \"email_id\"  : \"$email\",\n  \"full_name\" : \"$name\" ,\n  \"college_name\" : \" $college\" ,\n  \"refferal_code\" : \"$referral\" ,\n  \"years_of_study\" : \"$years\"\n}"
+                val body = data.toRequestBody(mediaType)
+                val request = Request.Builder()
+                    .url("https://backend.anwesha.live/campasambassador/register")
+                    .post(body)
+                    .addHeader("Content-Type", "application/json")
+                    .build()
+
+                try {
+                    val response = client.newCall(request).execute()
+                    if (response.isSuccessful)
+                        showFragment()
+                    else
+                        Snackbar.make(view, "Could not verify the user!", Snackbar.LENGTH_LONG)
+                            .setAnimationMode(ANIMATION_MODE_SLIDE).show()
+
+                } catch (e: Exception) {
+                    Snackbar.make(
+                        view,
+                        "Oops! It seems like an error... ${e.message}",
+                        Snackbar.LENGTH_LONG
+                    )
+                        .setAnimationMode(ANIMATION_MODE_SLIDE).show()
+                }
+            }.start()
+
         }
 
         return view
     }
+
+    private fun checkValue(input: TextInputLayout): String? {
+        val value = input.editText?.text.toString()
+        if(value == "") {
+            input.error = "Required Field!"
+            return null
+        }
+        input.error = null
+        return value
+    }
+
+
     private fun showFragment() {
         val fram = activity?.supportFragmentManager?.beginTransaction()
         fram?.replace(R.id.CaContainer, CaStatusFragment())
