4eb59cb8af77e8da962b6815a8ab60088d15edfd
Arvind <70971577+arvindri2005@users.noreply.github.com>
2023-03-17 02:29:16 +0530

    Passes added in profile
    * Some ui Changes in profile fragment
    
    * Passes added in profile

--- app/src/main/java/com/iitp/anwesha/profile/ProfileFragment.kt
+++ app/src/main/java/com/iitp/anwesha/profile/ProfileFragment.kt
@@ -4,8 +4,11 @@ import android.R
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
+import android.content.Intent
 import android.graphics.drawable.Drawable
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Browser
 import android.text.InputType
 import android.util.Log
 import android.view.LayoutInflater
@@ -30,6 +33,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.lang.reflect.Type
+import java.util.HashSet
 
 
 class ProfileFragment(context: Context) : Fragment() {
@@ -76,6 +80,7 @@ class ProfileFragment(context: Context) : Fragment() {
             regenerate()
         }
 
+
         CoroutineScope(Dispatchers.IO).launch {
             val response = UserProfileApi(requireContext()).profileApi.getProfile()
             if (response.isSuccessful) {
@@ -113,12 +118,65 @@ class ProfileFragment(context: Context) : Fragment() {
                     val eventsInfo = response2.body()!!
                     Log.e("PRINT", eventsInfo.toString())
                     val soloEvents = arrayListOf<MyEventDetails>()
+                    var l =1
                     for(i in eventsInfo.solo){
-                        if (i.event_tags!="6"){
-                            soloEvents.add(i)
+                        if (i.event_tags=="6"){
+                            if (l==1){
+                                if(i.event_name=="Fest Entry *Day 3"){
+                                    binding.passId1.text = "Pro Pass"
+                                }
+                                else{
+                                    binding.passId1.text = "Elite Pass"
+                                }
+
+                                if(!i.payment_done){
+                                    binding.paymentBtn1.visibility = View.VISIBLE
+                                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(i.payment_url))
+                                    val headers = Bundle()
+                                    val sharedPref = requireActivity().getSharedPreferences("UserPreferences", Context.MODE_PRIVATE)
+                                    var cookieString = ""
+                                    for(cookie in sharedPref.getStringSet(getString(com.iitp.anwesha.R.string.cookies), HashSet())!!) {
+                                        cookieString += "$cookie; "
+                                    }
+                                    headers.putString("Set-Cookie", cookieString)
+                                    intent.putExtra(Browser.EXTRA_HEADERS, headers)
+                                    binding.paymentBtn1.setOnClickListener {
+                                        startActivity(intent)
+                                    }
+                                }
+                                l++
+                                binding.pass1.visibility = View.VISIBLE
+                            }
+                            else{
+                                if(i.event_name=="Fest Entry *Day 3"){
+                                    binding.passId2.text = "Pro Pass"
+                                }
+                                else{
+                                    binding.passId2.text = "Elite Pass"
+                                }
+                                if(!i.payment_done){
+                                    binding.paymentBtn2.visibility = View.VISIBLE
+                                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(i.payment_url))
+                                    val headers = Bundle()
+                                    val sharedPref = requireActivity().getSharedPreferences("UserPreferences", Context.MODE_PRIVATE)
+                                    var cookieString = ""
+                                    for(cookie in sharedPref.getStringSet(getString(com.iitp.anwesha.R.string.cookies), HashSet())!!) {
+                                        cookieString += "$cookie; "
+                                    }
+                                    headers.putString("Set-Cookie", cookieString)
+                                    intent.putExtra(Browser.EXTRA_HEADERS, headers)
+                                    binding.paymentBtn2.setOnClickListener {
+                                        startActivity(intent)
+                                    }
+                                }
+                                l++
+                                binding.pass2.visibility = View.VISIBLE
+                            }
                         }
                         else{
-                            passes.add(i)
+                            soloEvents.add(i)
+
+
                         }
 
                     }
@@ -132,6 +190,34 @@ class ProfileFragment(context: Context) : Fragment() {
                     binding.rvTeamAdapter.adapter =
                         ProfileTeamsAdapter(eventsInfo.team, fragmentContext)
                 }
+            }
+        }
+
+
+        var l=1
+        for(i in passes){
+            if(l==1){
+                binding.passId1.text = i.event_name
+                if(!i.payment_done){
+                    binding.paymentBtn1.visibility = View.VISIBLE
+                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(i.payment_url))
+                    val headers = Bundle()
+                    val sharedPref = requireActivity().getSharedPreferences("UserPreferences", Context.MODE_PRIVATE)
+                    var cookieString = ""
+                    for(cookie in sharedPref.getStringSet(getString(com.iitp.anwesha.R.string.cookies), HashSet())!!) {
+                        cookieString += "$cookie; "
+                    }
+                    headers.putString("Set-Cookie", cookieString)
+                    intent.putExtra(Browser.EXTRA_HEADERS, headers)
+                    binding.paymentBtn1.setOnClickListener {
+                        startActivity(intent)
+                    }
+                }
+                l++
+                binding.pass1.visibility = View.VISIBLE
+            }
+            else{
+
             }
         }
 
