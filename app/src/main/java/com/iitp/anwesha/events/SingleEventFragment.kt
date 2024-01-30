07a4848eb0f5f8a3e6e8a624c2fc94e4af6b6118
Arvind <70971577+arvindri2005@users.noreply.github.com>
2024-01-31 00:03:28 +0530

    Added event register functionality (#119)

--- app/src/main/java/com/iitp/anwesha/events/SingleEventFragment.kt
+++ app/src/main/java/com/iitp/anwesha/events/SingleEventFragment.kt
@@ -1,10 +1,8 @@
 package com.iitp.anwesha.events
 
-import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.provider.Browser
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -17,16 +15,24 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.iitp.anwesha.R
 import com.iitp.anwesha.databinding.FragmentSingleEventBinding
 import com.iitp.anwesha.home.EventList
+import com.iitp.anwesha.profile.UserProfileApi
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.text.SimpleDateFormat
-import java.util.*
+import java.util.Locale
+import java.util.Random
+import java.util.TimeZone
 
-
+private const val TAG = "SingleEventFragment"
 class SingleEventFragment : Fragment() {
     private lateinit var binding: FragmentSingleEventBinding
     private lateinit var eventId: String
+    private lateinit var name: String
+    private lateinit var email: String
+    private lateinit var phone: String
+    private lateinit var anweshaId: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,28 +41,61 @@ class SingleEventFragment : Fragment() {
     }
 
     override fun onResume() {
+
         super.onResume()
-        val bottomNavigationView =
-            activity?.findViewById<BottomNavigationView>(R.id.bottomNavigation)
+
+        val bottomNavigationView = activity?.findViewById<BottomNavigationView>(R.id.bottomNavigation)
         bottomNavigationView?.visibility = View.GONE
+
     }
 
     override fun onDestroy() {
+
         super.onDestroy()
-        val bottomNavigationView =
-            activity?.findViewById<BottomNavigationView>(R.id.bottomNavigation)
+
+        val bottomNavigationView = activity?.findViewById<BottomNavigationView>(R.id.bottomNavigation)
         bottomNavigationView?.visibility = View.VISIBLE
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+
         binding = FragmentSingleEventBinding.inflate(inflater, container, false)
 
-        if (isAdded) {
-            val event = arguments?.getSerializable("event") as EventList
+        CoroutineScope(Dispatchers.IO).launch {
 
+            val response = UserProfileApi(requireContext()).profileApi.getProfile()
+
+            if (response.isSuccessful) {
+
+                val userInfo = response.body()!!
+                Log.d(TAG, "User Profile Response: ${response.body()}")
+
+                withContext(Dispatchers.Main) {
+                    name = userInfo.full_name
+                    email = userInfo.email_id
+                    phone = userInfo.phone_number
+                    anweshaId = userInfo.anwesha_id
+                }
+
+            } else {
+
+                withContext(Dispatchers.Main) {
+
+                    Log.d(TAG, "Error in User Profile Response: ${response.message()}")
+                    Toast.makeText(context, "error found: ${response.message()}", Toast.LENGTH_SHORT).show()
+
+                }
+            }
+        }
+
+        if (isAdded) {
+
+            val event = arguments?.getSerializable("event") as EventList
+            Log.d(TAG, "Event: $event")
 
             Glide.with(requireContext())
                 .load(event.poster)
@@ -76,7 +115,6 @@ class SingleEventFragment : Fragment() {
             val endTimeSeparatedString1 = endDateString.split(",")
             binding.eventDate.text =
                 startTimeSeparatedStrings[0] + " - " + endTimeSeparatedString1[0]
-//            binding.eventStartTime.text = separatedStrings[1]
 
             binding.eventDescription.text = event.description
             if (event.is_solo!!) {
@@ -91,6 +129,7 @@ class SingleEventFragment : Fragment() {
             if (event.registration_deadline==null){
                 binding.registerDeadline.visibility = View.GONE
             }
+
             else{
                 val endTime = event.registration_deadline
                 val endDate = inputFormat.parse(endTime)!!
@@ -107,6 +146,7 @@ class SingleEventFragment : Fragment() {
             for (string in organizerT) {
                 organizer = organizer+string[0] + " " +string[1]  +"\n"
             }
+
             binding.organizer.text = organizer
 
 
@@ -116,57 +156,68 @@ class SingleEventFragment : Fragment() {
             if(!event.is_active!!){
                 binding.registerBtn.visibility = View.GONE
             }
+
             binding.registerBtn.setOnClickListener {
                 if(event.is_active){
-                    if(event.is_online!!){
-                        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(event.registration_link))
-                        startActivity(intent)
+
+                    if(event.is_solo){
+
+                        Log.d(TAG, "Solo Event Registration")
+                        pay(event.registration_fee!!)
+
                     }
-                    else{
-                        if(event.is_solo){
 
-                            pay(event.registration_fee!!)
+                    else {
 
-                        }
-                        else {
-                            val minTeamMembers = event.min_team_size
-                            val maxTeamMembers = event.max_team_size
-                            val bundle = Bundle()
-                            bundle.putInt("minTeamMembers", minTeamMembers!!)
-                            bundle.putInt("maxTeamMembers", maxTeamMembers!!)
-                            bundle.putString("eventName", event.name)
-                            bundle.putString("eventID", event.id)
-                            bundle.putString("eventFee", event.registration_fee!!)
-                            val teamEventFragment = TeamEventFragment()
-                            teamEventFragment.arguments = bundle
-                            val fragmentManager = requireActivity().supportFragmentManager.beginTransaction()
-                            fragmentManager.addToBackStack(null)
-                            fragmentManager.replace(R.id.fragmentContainer, teamEventFragment)
-                            fragmentManager.commit()
-                        }
+                        val bundle = Bundle()
+                        bundle.putInt("minTeamMembers", event.min_team_size!!)
+                        bundle.putInt("maxTeamMembers", event.max_team_size!!)
+                        bundle.putString("eventName", event.name)
+                        bundle.putString("eventID", event.id)
+                        bundle.putString("eventFee", event.registration_fee!!)
+
+                        Log.d(TAG, "Starting Team Event Registration")
+                        val teamEventFragment = TeamEventFragment()
+                        teamEventFragment.arguments = bundle
+                        val fragmentManager = requireActivity().supportFragmentManager.beginTransaction()
+                        fragmentManager.addToBackStack(null)
+                        fragmentManager.replace(R.id.fragmentContainer, teamEventFragment)
+                        fragmentManager.commit()
 
                     }
                 }
             }
 
             if (event.video!!.isEmpty()) {
+
                 binding.rulebookBtn.visibility = View.GONE
+
             }
+
             binding.rulebookBtn.setOnClickListener {
+
                 val intent = Intent(Intent.ACTION_VIEW, Uri.parse(event.video))
                 startActivity(intent)
+
             }
 
         }
         binding.backBtn.setOnClickListener {
+
             requireActivity().supportFragmentManager.popBackStack(null, 0)
+
         }
 
         return binding.root
     }
 
     private fun pay(price: String){
-        Toast.makeText(requireContext(), "test", Toast.LENGTH_SHORT).show()
+
+        val transactionId = generateTransactionId()
+        Log.d(TAG, "Transition ID: $transactionId")
+
+        Toast.makeText(requireContext(), "Please Wait", Toast.LENGTH_SHORT).show()
+
         val newPayIntent = Intent(requireContext(), PayActivity::class.java)
         newPayIntent.putExtra("merchantId", "564719")
         newPayIntent.putExtra("password", "b5d2bc5e")
@@ -174,7 +225,7 @@ class SingleEventFragment : Fragment() {
         newPayIntent.putExtra("txncurr", "INR")
         newPayIntent.putExtra("custacc", "100000036600")
         newPayIntent.putExtra("amt", price)
-        newPayIntent.putExtra("txnid", "txnfeb2023")
+        newPayIntent.putExtra("txnid",transactionId)
         newPayIntent.putExtra("signature_request", "KEY1234567234")
         newPayIntent.putExtra("signature_response", "KEYRESP123657234")
         newPayIntent.putExtra("enc_request", "1E67285F56177ADD96D6453F90482D12")
@@ -182,54 +233,82 @@ class SingleEventFragment : Fragment() {
         newPayIntent.putExtra("salt_response", "66F34D46E547C535047F3465E640F32B")
         newPayIntent.putExtra("enc_response", "66F34D46E547C535047F3465E640F32B")
         newPayIntent.putExtra("isLive", true)
-        newPayIntent.putExtra("custFirstName", "test user")
-        newPayIntent.putExtra("customerEmailID", "test@gmail.com")
-        newPayIntent.putExtra("customerMobileNo", "8888888888")
-        newPayIntent.putExtra("udf1", "udf1")
-        newPayIntent.putExtra("udf2", "udf2")
-        newPayIntent.putExtra("udf3", "udf3")
+        newPayIntent.putExtra("custFirstName", name)
+        newPayIntent.putExtra("customerEmailID", email)
+        newPayIntent.putExtra("customerMobileNo", phone)
+        newPayIntent.putExtra("AnweshaId", anweshaId)
+        newPayIntent.putExtra("eventId", eventId)
+        newPayIntent.putExtra("transactionId", transactionId)
         newPayIntent.putExtra("udf4", "udf4")
         newPayIntent.putExtra("udf5", "udf5")
+
         startActivityForResult(newPayIntent, 1)
     }
 
+
+
     @Deprecated("Deprecated in Java")
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
         super.onActivityResult(requestCode, resultCode, data)
         println("resultCode = $resultCode")
         println("onActivityResult data = $data")
+
         if (data != null && resultCode != 2) {
+
             println("ArrayList data = " + data.extras!!.getString("response"))
+
             if (resultCode == 1) {
-                Toast.makeText(requireContext(), "Transaction Successful! \n" + data.extras!!.getString("response"), Toast.LENGTH_LONG).show()
-                CoroutineScope(Dispatchers.IO).launch {
-                    try {
-                        Log.d("log","128")
-                        val response1 = EventsRegistrationApi(requireContext()).allEventsApi.soloEventRegistration(SoloRegistration(eventId))
-                        Log.d("response", "130")
-                        if(response1.isSuccessful){
-                            requireActivity().runOnUiThread {
-                                Toast.makeText(requireContext(), "Registered Successfully", Toast.LENGTH_SHORT).show()
-                            }
-                        }
-                        else{
-                            Log.d("e", "${response1.errorBody()}")
-                        }
-                    }
-                    catch (e: Exception){
-                        Log.d("Error", e.toString())
-                    }
-                }
+
+                Log.d(TAG, "Transaction Successful! \n" + data.extras!!.getString("response"))
+                Toast.makeText(requireContext(), "Transaction Successful!", Toast.LENGTH_LONG).show()
+
+                registerEvent()
+
             } else {
-                Toast.makeText(
-                    requireContext(),
-                    "Transaction Failed! \n" + data.extras!!.getString("response"),
-                    Toast.LENGTH_LONG)
-                    .show()
+
+                Log.d(TAG, "Transaction Failed! \n" + data.extras!!.getString("response"))
+                Toast.makeText(requireContext(), "Transaction Failed!", Toast.LENGTH_LONG).show()
+
             }
         } else {
-            Toast.makeText(requireContext(), "Transaction Cancelled!", Toast.LENGTH_LONG).show()
-        }
-    } // onActivityResult
 
+            Log.d(TAG, "Transaction Cancelled!")
+            Toast.makeText(requireContext(), "Transaction Cancelled!", Toast.LENGTH_LONG).show()
+
+        }
+    }
+
+    private fun registerEvent(){
+        CoroutineScope(Dispatchers.IO).launch {
+
+            try {
+
+                val response1 = EventsRegistrationApi(requireContext()).allEventsApi.soloEventRegistration(SoloRegistration(eventId))
+
+                if(response1.isSuccessful){
+
+                    requireActivity().runOnUiThread {
+                        Toast.makeText(requireContext(), "Registered Successfully", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+                else{
+                    Log.d(TAG, "${response1.errorBody()}")
+                }
+            }
+
+            catch (e: Exception){
+                Log.d(TAG, e.toString())
+            }
+        }
+    }
+}
+
+fun generateTransactionId(): String {
+    val chars = "abcdefghijklmnopqrstuvwxyz0123456789"
+    return (1..12)
+        .map { Random().nextInt(chars.length) }
+        .map(chars::get)
+        .joinToString("")
 }
--- app/src/main/java/com/iitp/anwesha/events/TeamEventFragment.kt
+++ app/src/main/java/com/iitp/anwesha/events/TeamEventFragment.kt
@@ -20,9 +20,11 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.iitp.anwesha.R
 import com.iitp.anwesha.databinding.FragmentTeamEventBinding
 import com.iitp.anwesha.databinding.TeamMemberFieldBinding
+import com.iitp.anwesha.profile.UserProfileApi
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.util.HashSet
 
 
@@ -31,6 +33,7 @@ private const val ARG_PARAM2 = "maxTeamMembers"
 private const val ARG_PARAM3 = "eventName"
 private const val ARG_PARAM4 = "eventID"
 private const val ARG_PARAM5 = "eventFee"
+private const val TAG = "TeamEventFragment"
 
 
 class TeamEventFragment : Fragment() {
@@ -43,10 +46,15 @@ class TeamEventFragment : Fragment() {
     private lateinit var binding: FragmentTeamEventBinding
     private var counter: Int = 1
     private lateinit var teamList: ArrayList<String>
+    private lateinit var name: String
+    private lateinit var email: String
+    private lateinit var phone: String
+    private lateinit var anweshaId: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
+
             minTeamMembers = it.getInt(ARG_PARAM1)
             maxTeamMembers = it.getInt(ARG_PARAM2)
             eventName = it.getString(ARG_PARAM3)
@@ -59,16 +67,45 @@ class TeamEventFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
+    ): View {
+
         binding = FragmentTeamEventBinding.inflate(inflater, container, false)
+
+        CoroutineScope(Dispatchers.IO).launch {
+
+            val response = UserProfileApi(requireContext()).profileApi.getProfile()
+
+            if (response.isSuccessful) {
+
+                val userInfo = response.body()!!
+                Log.d(TAG,"User info: $userInfo")
+
+                withContext(Dispatchers.Main) {
+
+                    name = userInfo.full_name
+                    email = userInfo.email_id
+                    phone = userInfo.phone_number
+                    anweshaId = userInfo.anwesha_id
+
+                }
+            } else {
+                withContext(Dispatchers.Main) {
+
+                    Log.d(TAG,"Error in getting profile info: ${response.message()}")
+                    Toast.makeText(context, "error found: ${response.message()}", Toast.LENGTH_SHORT).show()
+
+                }
+            }
+        }
         return binding.root
     }
     override fun onResume() {
+
         super.onResume()
         val bottomNavigationView =
             activity?.findViewById<BottomNavigationView>(R.id.bottomNavigation)
         bottomNavigationView?.visibility = View.GONE
+
     }
 
     override fun onDestroy() {
@@ -86,14 +123,11 @@ class TeamEventFragment : Fragment() {
         for (i in 1..minTeamMembers!!) {
 
             val teamMember = layoutInflater.inflate(R.layout.team_member_field, null)
-//            val teamMember = View.inflate(context, R.layout.team_member_field, binding.teamMembers)
             teamMember.findViewById<ImageView>(R.id.delete_team_member).visibility = View.INVISIBLE
             teamMember.findViewById<TextView>(R.id.team_member_index).text = counter.toString()
-//            if(teamMember.parent != null) {
-//                (teamMember.parent as ViewGroup).removeView(teamMember)
-//            }
             counter++
             binding.teamMembers.addView(teamMember)
+
         }
 
         binding.backBtn.setOnClickListener {
@@ -101,48 +135,57 @@ class TeamEventFragment : Fragment() {
         }
 
         binding.addMembers.setOnClickListener {
+
             if (counter >= maxTeamMembers!!) {
                 Toast.makeText(context, "Max participant!", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
+
             val teamMember = layoutInflater.inflate(R.layout.team_member_field, null)
             teamMember.findViewById<ImageView>(R.id.delete_team_member).setOnClickListener {
+
                 binding.teamMembers.removeView(teamMember)
                 counter--
-                val members = binding.teamMembers.childCount
+
                 var i = 1
                 for (child in binding.teamMembers.children) {
                     child.findViewById<TextView>(R.id.team_member_index).text = i.toString()
                     i++
                 }
+
             }
+
             teamMember.findViewById<TextView>(R.id.team_member_index).text = counter.toString()
             binding.teamMembers.addView(teamMember)
             counter++
         }
 
         binding.teamRegister.setOnClickListener {
+
             teamList = arrayListOf<String>()
             var count = 0
+
             for (child in binding.teamMembers.children) {
                 val memberName = child.findViewById<EditText>(R.id.team_member_id).text.toString()
                 if(memberName.isNotEmpty()) count++
                 teamList.add(memberName)
             }
+
             if(count < minTeamMembers!! || binding.teamName.text.toString().isEmpty()) {
                 Toast.makeText(context, "Please enter all the fields", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
 
             pay(eventFee!!)
-
-
-
-
         }
     }
     private fun pay(price: String){
+
+        val transactionId = generateTransactionId()
+        Log.d(TAG, "Transaction id: $transactionId")
+
         Toast.makeText(requireContext(), "test", Toast.LENGTH_SHORT).show()
+
         val newPayIntent = Intent(requireContext(), PayActivity::class.java)
         newPayIntent.putExtra("merchantId", "564719")
         newPayIntent.putExtra("password", "b5d2bc5e")
@@ -150,7 +193,7 @@ class TeamEventFragment : Fragment() {
         newPayIntent.putExtra("txncurr", "INR")
         newPayIntent.putExtra("custacc", "100000036600")
         newPayIntent.putExtra("amt", price)
-        newPayIntent.putExtra("txnid", "txnfeb2023")
+        newPayIntent.putExtra("txnid", transactionId)
         newPayIntent.putExtra("signature_request", "KEY1234567234")
         newPayIntent.putExtra("signature_response", "KEYRESP123657234")
         newPayIntent.putExtra("enc_request", "1E67285F56177ADD96D6453F90482D12")
@@ -158,73 +201,84 @@ class TeamEventFragment : Fragment() {
         newPayIntent.putExtra("salt_response", "66F34D46E547C535047F3465E640F32B")
         newPayIntent.putExtra("enc_response", "66F34D46E547C535047F3465E640F32B")
         newPayIntent.putExtra("isLive", true)
-        newPayIntent.putExtra("custFirstName", "test user")
-        newPayIntent.putExtra("customerEmailID", "test@gmail.com")
-        newPayIntent.putExtra("customerMobileNo", "8888888888")
-        newPayIntent.putExtra("udf1", "udf1")
-        newPayIntent.putExtra("udf2", "udf2")
-        newPayIntent.putExtra("udf3", "udf3")
+        newPayIntent.putExtra("custFirstName", name)
+        newPayIntent.putExtra("customerEmailID", email)
+        newPayIntent.putExtra("customerMobileNo", phone)
+        newPayIntent.putExtra("AnweshaId", anweshaId)
+        newPayIntent.putExtra("eventId", eventID)
+        newPayIntent.putExtra("transactionId", transactionId)
         newPayIntent.putExtra("udf4", "udf4")
         newPayIntent.putExtra("udf5", "udf5")
+
         startActivityForResult(newPayIntent, 1)
     }
 
     @Deprecated("Deprecated in Java")
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
         super.onActivityResult(requestCode, resultCode, data)
         println("resultCode = $resultCode")
         println("onActivityResult data = $data")
+
         if (data != null && resultCode != 2) {
+
             println("ArrayList data = " + data.extras!!.getString("response"))
             if (resultCode == 1) {
-                Toast.makeText(requireContext(), "Transaction Successful! \n" + data.extras!!.getString("response"), Toast.LENGTH_LONG).show()
-                CoroutineScope(Dispatchers.IO).launch {
-                    try {
-                        CoroutineScope(Dispatchers.IO).launch {
-                            val teamRegistration =
-                                TeamRegistration(eventID!!, binding.teamName.text.toString(), teamList)
-                            val response =
-                                EventsRegistrationApi(requireContext()).allEventsApi.teamEventRegistration(
-                                    teamRegistration
-                                )
-                            if (response.isSuccessful) {
-                                val teamReg = response.body()!!
-                                requireActivity().runOnUiThread {
-                                    if (teamReg.message == null) {
-                                        Toast.makeText(
-                                            requireContext(),
-                                            "Already resgisterd",
-                                            Toast.LENGTH_SHORT
-                                        ).show()
 
+                Log.d(TAG, "Transaction Successful! \n" + data.extras!!.getString("response"))
+                Toast.makeText(requireContext(), "Transaction Successful!", Toast.LENGTH_LONG).show()
 
-                                        return@runOnUiThread
-                                    } else {
+                registerEvent()
 
-                                    }
-                                }
-                            }
-                            else {
-                                requireActivity().runOnUiThread {
-                                    Toast.makeText(context, "Invalid details", Toast.LENGTH_SHORT).show()
-                                }
-                            }
-                        }
-                    }
-                    catch (e: Exception){
-                        Log.d("Error", e.toString())
-                    }
-                }
             } else {
-                Toast.makeText(
-                    requireContext(),
-                    "Transaction Failed! \n" + data.extras!!.getString("response"),
-                    Toast.LENGTH_LONG)
-                    .show()
+
+                Log.d(TAG, "Transaction Failed! \n" + data.extras!!.getString("response"))
+                Toast.makeText(requireContext(), "Transaction Failed!", Toast.LENGTH_LONG).show()
+
             }
         } else {
+
+            Log.d(TAG, "Transaction Cancelled!")
             Toast.makeText(requireContext(), "Transaction Cancelled!", Toast.LENGTH_LONG).show()
+
         }
     } // onActivityResult
 
+    private fun registerEvent() {
+
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+
+                CoroutineScope(Dispatchers.IO).launch {
+
+                    val teamRegistration = TeamRegistration(eventID!!, binding.teamName.text.toString(), teamList)
+                    Log.d(TAG, "Team registration: $teamRegistration")
+
+                    val response = EventsRegistrationApi(requireContext()).allEventsApi.teamEventRegistration(teamRegistration)
+
+                    if (response.isSuccessful) {
+                        val teamReg = response.body()!!
+                        requireActivity().runOnUiThread {
+                            if (teamReg.message == null) {
+                                Toast.makeText(requireContext(), "Already resgisterd", Toast.LENGTH_SHORT).show()
+                                return@runOnUiThread
+
+                            } else {
+                                Toast.makeText(requireContext(), "Registered successfully", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    }
+                    else {
+                        requireActivity().runOnUiThread {
+                            Toast.makeText(context, "Invalid details", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+            }
+
+            catch (e: Exception){
+                Log.d(TAG, e.toString())
+            }
+        }
+    }
 }
